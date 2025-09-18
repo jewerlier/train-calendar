06d021a117df3341101a1f7d20bc87341c8ff1ee
jewerlier <a23171475@gmail.com>
2025-09-18 23:34:40 +0200

    Fix manifest to webmanifest and update index.html

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('workouts-v1').then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.webmanifest',
+    caches.open('workouts-v2').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './icon-192.png',
+      './icon-512.png',
+      './favicon.ico'
     ]))
+  );
+  self.skipWaiting(); // Заставляет новый SW активироваться сразу
+});
+
+self.addEventListener('activate', e => {
+  const cacheWhitelist = ['workouts-v2'];
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // Удаляет старые кэши
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Активирует SW для всех клиентов
   );
 });
 
